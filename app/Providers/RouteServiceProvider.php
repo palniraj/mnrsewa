170de3e3a14b60553eeae3f2e360f7d2637033a8
palniraj <palniraj21@gmail.com>
2022-03-28 08:47:26 +0545

    file dir changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/cmspanel';
+    public const HOME = '/web-panel';
 
     /**
      * The controller namespace for the application.
